35e1042113f556bdab3090744843b382a25c2a4b
Joel Reis <jreis@imaginarycloud.com>
2020-04-22 15:52:46 +0100

    Prettier config v1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+}
